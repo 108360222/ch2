8332a3cfc9b7ec91c766bda962f8d7c892547884
108360222 <liujimmy20010715@gmail.com>
2019-10-18 20:12:27 +0800

    ch2

---
+++ lab1-2/source/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	Han_Xin_1();
+	Han_Xin_2();
+	Han_Xin_3();
+	Han_Xin_4();
+	Han_Xin_5();
+
+	printf("\n");
+	system("pause");
+	return 0;
+}
